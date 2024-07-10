98b547488aa84e0acd3927cf1d6a0e3a1d4ff356
Sam Kearney <samuel.kearney@taittowers.com>
2024-07-09 20:13:32 -0500

    Add client certificate verification

--- arbiter/src/main.rs
+++ arbiter/src/main.rs
@@ -2,11 +2,11 @@ use std::{fs::File, io::BufReader};
 
 use coap::Server;
 use rcgen::KeyPair;
-use rustls::Certificate as RustlsCertificate;
+use rustls::{Certificate as RustlsCertificate, RootCertStore};
 use tokio::sync::mpsc::channel;
 use webrtc_dtls::{
-    config::Config as DtlsConfig,
-    crypto::{Certificate, CryptoPrivateKey, CryptoPrivateKeyKind},
+    config::{ClientAuthType, Config as DtlsConfig},
+    crypto::{Certificate, CryptoPrivateKey},
     listener::listen,
 };
 
@@ -20,25 +20,13 @@ mod state;
 async fn main() {
     let addr = "127.0.0.1:5683";
 
-    let private_key = std::fs::read_to_string("../certs/arbiter-key.pem").unwrap();
-    let private_key = KeyPair::from_pem(&private_key).unwrap();
-    let private_key = CryptoPrivateKey::from_key_pair(&private_key).unwrap();
-
-    let certs: Vec<_> = rustls_pemfile::certs(&mut BufReader::new(
-        File::open("../certs/arbiter-cert.pem").unwrap(),
-    ))
-    .map(|cert_result| RustlsCertificate(cert_result.unwrap().to_vec()))
-    .collect();
-
-    let certificates = vec![Certificate {
-        certificate: certs,
-        private_key,
-    }];
+    let client_cas = get_root_cert_store();
+    let certificates = get_my_certs();
 
     let config = DtlsConfig {
         certificates,
-        // client_auth: ClientAuthType::RequireAndVerifyClientCert
-        // client_cas:
+        client_auth: ClientAuthType::RequireAndVerifyClientCert,
+        client_cas,
         server_name: "arbiter.local".into(),
         ..Default::default()
     };
@@ -55,4 +43,33 @@ async fn main() {
     server.run(RequestHandler::new(tx)).await.unwrap();
 
     state_handle.await.unwrap();
+}
+
+fn get_root_cert_store() -> RootCertStore {
+    let mut store = RootCertStore::empty();
+    for cert in rustls_pemfile::certs(&mut BufReader::new(
+        File::open("../certs/root-cert.pem").unwrap(),
+    )) {
+        store
+            .add(&RustlsCertificate(cert.unwrap().to_vec()))
+            .unwrap();
+    }
+    store
+}
+
+fn get_my_certs() -> Vec<Certificate> {
+    let private_key = std::fs::read_to_string("../certs/arbiter-key.pem").unwrap();
+    let private_key = KeyPair::from_pem(&private_key).unwrap();
+    let private_key = CryptoPrivateKey::from_key_pair(&private_key).unwrap();
+
+    let certs: Vec<_> = rustls_pemfile::certs(&mut BufReader::new(
+        File::open("../certs/arbiter-cert.pem").unwrap(),
+    ))
+    .map(|cert_result| RustlsCertificate(cert_result.unwrap().to_vec()))
+    .collect();
+
+    vec![Certificate {
+        certificate: certs,
+        private_key,
+    }]
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -4,10 +4,12 @@ use std::{fs::File, io::BufReader};
 use coap::client::CoAPClient;
 use coap::dtls::UdpDtlsConfig;
 use coap::request::{Method, RequestBuilder};
-use rustls::{Certificate, RootCertStore};
+use rcgen::KeyPair;
+use rustls::{Certificate as RustlsCertificate, RootCertStore};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use webrtc_dtls::config::Config as DtlsConfig;
+use webrtc_dtls::crypto::{Certificate, CryptoPrivateKey};
 
 #[derive(Serialize)]
 struct PutDevicePayload {
@@ -28,21 +30,13 @@ pub struct ApiDevice {
 
 #[tokio::main]
 async fn main() {
-    let mut my_store = RootCertStore::empty();
-
-    let certs: Vec<_> = rustls_pemfile::certs(&mut BufReader::new(
-        File::open("../certs/root-cert.pem").unwrap(),
-    ))
-    .map(|cert_result| Certificate(cert_result.unwrap().to_vec()))
-    .collect();
-
-    for cert in certs {
-        my_store.add(&cert).unwrap();
-    }
+    let roots_cas = get_root_cert_store();
+    let certificates = get_my_certs();
 
     let config = DtlsConfig {
+        certificates,
         server_name: "arbiter.local".into(),
-        roots_cas: my_store,
+        roots_cas,
         ..Default::default()
     };
     let config = UdpDtlsConfig {
@@ -83,4 +77,33 @@ async fn main() {
     let response = client.send(request).await.unwrap();
     let devices: Vec<ApiDevice> = serde_json::from_slice(&response.message.payload).unwrap();
     println!("Server reply: {:?}", devices);
+}
+
+fn get_root_cert_store() -> RootCertStore {
+    let mut store = RootCertStore::empty();
+    for cert in rustls_pemfile::certs(&mut BufReader::new(
+        File::open("../certs/root-cert.pem").unwrap(),
+    )) {
+        store
+            .add(&RustlsCertificate(cert.unwrap().to_vec()))
+            .unwrap();
+    }
+    store
+}
+
+fn get_my_certs() -> Vec<Certificate> {
+    let private_key = std::fs::read_to_string("../certs/client-key.pem").unwrap();
+    let private_key = KeyPair::from_pem(&private_key).unwrap();
+    let private_key = CryptoPrivateKey::from_key_pair(&private_key).unwrap();
+
+    let certs: Vec<_> = rustls_pemfile::certs(&mut BufReader::new(
+        File::open("../certs/client-cert.pem").unwrap(),
+    ))
+    .map(|cert_result| RustlsCertificate(cert_result.unwrap().to_vec()))
+    .collect();
+
+    vec![Certificate {
+        certificate: certs,
+        private_key,
+    }]
 }
